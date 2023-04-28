62f6d4ed144166a024fb8468b95536b4b335b742
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-04-28 18:32:34 +0545

    fetch-2 completed

---
+++ Fetch-2/fetch-requests.js
@@ -0,0 +1,30 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============== 1. Print the status code of the response =============== */
+
+// Your code here
+const url="/products";
+fetch(url).then(response=>console.log(response.status));
+
+
+/* ====== 2. Print true if the status of the response was successful ====== */
+
+// Your code here
+fetch(url).then(response=>console.log(response.ok));
+
+
+
+/* =================== 3. Print the Content-Type Header =================== */
+
+// Your code here
+fetch(url).then(response=>console.log(response.headers.get("Content-Type")));
+
+
+
+/* ============== 4. Print the body of the response as text =============== */
+
+// Your code here
+fetch(url).then(response=>console.log(response.text()));
