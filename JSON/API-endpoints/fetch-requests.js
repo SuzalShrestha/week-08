617638736c508850df96b6ae64097d8dd7ef630c
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-04-29 19:44:51 +0545

    completed API-endpoints and Create-api

---
+++ JSON/API-endpoints/fetch-requests.js
@@ -0,0 +1,28 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============================== Phase 1 ================================ */
+/*
+  Make a request with fetch request to GET /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+fetch('/posts').then(res=>res.json).then(res=>console.log(res));
+
+
+
+/* =============================== Phase 2 ================================ */
+/*
+  Make a request with fetch request to POST /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+fetch('/posts', {
+  method:"POST",
+  headers: { "Content-Type": "application/json" },
+  body:JSON.stringify({message: "Hello I am human!"})
+});
