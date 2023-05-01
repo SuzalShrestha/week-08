34f64271d7145b7cf0734759adb100df247fb243
Sujal Shrestha <sujal.scxi121@amv.edu.np>
2023-05-01 14:17:26 +0545

    complted Music-archive-docs

---
+++ JSON/Music-archive-docs/s.js
@@ -0,0 +1,9 @@
+const url="/artists"
+const header={"Content-Type": "application/json"}
+const body=JSON.stringify({name:"Sia"})
+const options={
+    method:"POST",
+    headers:header,
+    body:body
+    
+}
